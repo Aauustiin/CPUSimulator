7f74cf9b1d212622b682fd4154ba7f92d7e7fd0c
Aauustiin <longaustin2001@gmail.com>
2023-03-31 13:58:13 +0100

    Did some command line args parsing

--- Assets/Scripts/ProcessorSimulator.cs
+++ Assets/Scripts/ProcessorSimulator.cs
@@ -1,11 +1,25 @@
 using System;
 
 public class ProcessorSimulator {
+    
+    private const string DefaultFilePath = "THING";
+    private const Mode DefaultMode = Mode.RELEASE;
+    
     private static void Main(string[] args)
     {
+        string filePath;
+        var a = Array.IndexOf(args, "-program");
+        if (a != -1) filePath = args[a + 1];
+        else filePath = DefaultFilePath;
+        
+        Mode mode;
+        var b = Array.IndexOf(args, "-mode");
+        if (b != -1) Enum.TryParse(args[b + 1], out mode);
+        else mode = DefaultMode;
+        
         Processor processor = new Processor(1, 16);
-        var program = Parsing.LoadProgram("put path here");
-        processor.Initialise(new int[128], program, Mode.RELEASE);
+        var program = Parsing.LoadProgram(filePath);
+        processor.Initialise(new int[128], program, mode);
         processor.Process();
     }
 }
