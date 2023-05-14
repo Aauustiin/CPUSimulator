d12af66223f93a67720819467413dcfd047b3973
Aauustiin <longaustin2001@gmail.com>
2023-05-14 19:46:10 +0100

    Planned initialisation form

---
+++ Assets/Scripts/InitialisationForm.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class InitialisationForm : MonoBehaviour
+{
+    private const string DefaultFilePath = "Assets\\Scripts\\Assembly\\Test\\Test.txt";
+
+    public void OnSubmit()
+    {
+        // Collect data from all your form fields.
+        // Create a processorSpecification
+        // Create a programSpecification
+        // Run the processor
+        // Close the form and open whatever screen you have for monitoring data
+    }
+}
