83c74cad469b518eb1d9ff64a5b3b49806e966a6
Aauustiin <longaustin2001@gmail.com>
2023-05-14 18:30:18 +0100

    rewrote reorder buffer issue

--- Assets/Scripts/ReorderBuffer.cs
+++ Assets/Scripts/ReorderBuffer.cs
@@ -3,25 +3,38 @@ using System.Linq;
 
 public class ReorderBuffer
 {
-    public readonly ReorderBufferEntry[] Entries;
-    private int _issuePointer, _commitPointer = -1;
-    private Processor _processor;
+    public ReorderBufferEntry[] Entries;
+    private int _issuePointer, _commitPointer;
+    private readonly Processor _processor;
     
-    public ReorderBuffer(int size)
+    public ReorderBuffer(int size, Processor processor)
     {
         Entries = new ReorderBufferEntry[size];
         for (var i = 0; i < size; i++)
         {
             Entries[i].Id = i;
+            Entries[i].Free = true;
         }
+
+        _commitPointer = -1;
+        _issuePointer = 0;
+        
+        _processor = processor;
+        _processor.BranchMispredict += OnBranchMispredict;
+    }
+
+    ~ReorderBuffer()
+    {
+        _processor.BranchMispredict -= OnBranchMispredict;
     }
 
     // This is for adding something to the Reorder Buffer
-    public void Issue(ReorderBufferEntry entry)
+    public void Issue(Opcode opcode, int fetchNum, int? destination, int? value)
     {
         if (IsFull()) return; // Can't issue anything if we don't have space.
-        Entries[_issuePointer] = entry;
-        _issuePointer = (_issuePointer + 1) % Entries.Length;
+        if (_commitPointer == -1) _commitPointer = _issuePointer;
+        Entries[_issuePointer].Initialise(opcode, fetchNum, destination, value);
+        _issuePointer = (_issuePointer - 1) % Entries.Length;
     }
     
     // Update a given entry with a new value and/or destination.
@@ -47,21 +60,46 @@ public class ReorderBuffer
     {
         return Entries.All(entry => entry.Free == false);
     }
+
+    private void OnBranchMispredict(int fetchNum)
+    {
+        var remainingEntries = Entries.Where(entry => entry.FetchNum < fetchNum).ToArray();
+        for (var i = 0; i < Entries.Length; i++)
+        {
+            Entries[i] = i < remainingEntries.Length ? remainingEntries[i] : new ReorderBufferEntry();
+            Entries[i].Id = i;
+        }
+
+        _commitPointer = remainingEntries.Length - 1;
+        _issuePointer = Entries.Length - 1;
+    }
 }
 
 public class ReorderBufferEntry
 {
+    public int Id;
     public bool Free;
+    
+    public Opcode Opcode;
     public int FetchNum;
+    
     private int? _destination;
     private int? _value;
-    public int Id;
-    public Opcode Opcode;
 
     public event System.Action<int> ValueProvided;
     
     public event System.Action<int> DestinationProvided;
 
+    public void Initialise(Opcode  opcode, int fetchNum, int? destination, int? value)
+    {
+        Free = false;
+        
+        Opcode = opcode;
+        FetchNum = fetchNum;
+        _destination = destination;
+        _value = value;
+    }
+    
     public void SetValue(int value)
     {
         _value = value;
@@ -82,14 +120,5 @@ public class ReorderBufferEntry
     public int? GetDestination()
     {
         return _destination;
-    }
-    
-    public ReorderBufferEntry(int destination, int value, Opcode opcode, int id, int fetchNum)
-    {
-        FetchNum = fetchNum;
-        _destination = destination;
-        _value = null;
-        Id = id;
-        Free = true;
     }
 }
